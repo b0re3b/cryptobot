b2b38433f1f4cba53c91d13c72d9c533409345b9
b0r3d <bogd.reschetko2017@gmail.com>
2025-04-14 15:51:11 +0300

    tested connection with telegram and generated api token

--- chat_bot/crypto_bot.py
+++ chat_bot/crypto_bot.py
@@ -1,1 +1,21 @@
-# Основна логіка крипто-чат-бота
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from utils.config import TELEGRAM_API
+
+bot = Bot(token=TELEGRAM_API)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(
+        "👋 Привіт!\n\n"
+        "Я бот, який допоможе тобі у трейдингу, прогнозуючи курс криптовалют 📈.\n"
+        "Вибери валюту, яка тебе цікавить, і часовий проміжок ⏳."
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
